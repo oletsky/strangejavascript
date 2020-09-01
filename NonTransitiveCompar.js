6a1a70156b1e8157ac060904618460fcf3014ecf
Oleksiy Oletsky <oletsky@ukr.net>
2020-09-01 10:28:54 +0300

    Add files via upload

---
+++ NonTransitiveCompar.js
@@ -0,0 +1,7 @@
+"use strict";
+let a="0";
+let b=0;
+let c=[];
+console.log(a==b); //true
+console.log(b==c); //true
+console.log(a==c); //false
---
+++ StrangePlusMinus.js
@@ -0,0 +1,8 @@
+"use strict";
+let s=1;
+console.log(s);
+let a='1';
+s=s+a;
+console.log(s); //11
+s=s-a;
+console.log(s); //10
