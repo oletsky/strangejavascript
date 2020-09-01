8845478f2d4c8a28a572bdc264fa48321344a60d
Oleksiy Oletsky <oletsky@ukr.net>
2020-09-01 10:17:01 +0300

    Add files via upload

---
+++ comparison.js
@@ -0,0 +1,4 @@
+"use strict";
+let x=NaN;
+let y =NaN;
+console.log(x==y);
---
+++ demonan.js
@@ -0,0 +1,9 @@
+"use strict";
+let x=NaN;
+let y=0/0;
+let z = 5/0;
+console.log(z);
+console.log(NaN);
+console.log(y); //NaN
+console.log(typeof y); //number
+console.log(x==x); //false
