e2a4449c323bd67b8a7701907de0d97fc539c29d
Oleksiy Oletsky <oletsky@ukr.net>
2020-09-16 16:39:03 +0300

    Add files via upload

---
+++ compartotrue.js
@@ -0,0 +1,8 @@
+"use strict";
+let a=2;
+if (a) console.log("true");
+else console.log("false"); //true
+if (a==true) console.log("true");
+else console.log("false"); //false
+console.log(Boolean(a)); //true
+console.log(Number(true)); //1
